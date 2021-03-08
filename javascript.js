71a5be36d75caf3b87d5df24eaa272b38fed927c
Bea <bebadmyweb@gmail.com>
2021-03-08 22:12:20 +0100

    pujar de nou exercici8

--- javascript.js
+++ javascript.js
@@ -1,19 +1,5 @@
 
-// Exercici 7
-let nums1 = prompt("Introdueix el primer número");
-let nums2 = prompt("Introdueix el segon número");
-let i;
-
-function Multiplicar(){
-    for(i=0;i<=nums2;i++){
-      res=i*nums1;
-      document.getElementById('multiplication').innerHTML +=`${nums1} x ${i} = ${res}<br>`;
-    }
-  }
-  Multiplicar();
- 
- 
-  //Exercici 8
+//Exercici 8
  
  // variables
  var num1 = document.getElementById('num1');
