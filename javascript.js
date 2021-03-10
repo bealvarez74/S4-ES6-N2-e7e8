967ffd51474d37da6efdc22c8313e116d972c59d
Bea <bebadmyweb@gmail.com>
2021-03-10 16:52:33 +0100

    primer commit

--- javascript.js
+++ javascript.js
@@ -1,14 +1,2 @@
 
-// Exercici 7
-let nums1 = prompt("Introdueix el primer número");
-let nums2 = prompt("Introdueix el segon número");
-let i
-
-function Multiplicar(){
-    for(i=0;i<=nums2;i++)
-    {
-      res=i*num1;
-      document.getElementById('multiplication').innerHTML+=`${nums1} x ${i} = ${res}<br>`;
-    }
-  }
-  Multiplicar();
+// Nivel 2
